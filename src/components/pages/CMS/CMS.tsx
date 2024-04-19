57c94c521de0ec336e0ae6dbd3edb8b124d12a41
Luan Nguyen Manh <luan.nguyen3@niteco.se>
2024-04-19 11:41:32 +0700

    Minor change.

--- src/components/pages/CMS/CMS.tsx
+++ src/components/pages/CMS/CMS.tsx
@@ -7,8 +7,6 @@ export const CMSPage = ({
     return (
         <>
             <HeroCarousel />
-<!--             <div dangerouslySetInnerHTML={{__html: content}}></div>
-            <div dangerouslySetInnerHTML={{__html: bottom_content}}></div> -->
         </>
     )
 }
