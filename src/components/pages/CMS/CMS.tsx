4957c144ee65b23c22f421676d3b7ab415c1c132
Luan Nguyen Manh <luan.nguyen3@niteco.se>
2024-04-22 18:44:36 +0700

    Include the carousel back.

--- src/components/pages/CMS/CMS.tsx
+++ src/components/pages/CMS/CMS.tsx
@@ -1,10 +1,12 @@
 import { IRoute } from "@/hooks/resolveURL/resolveURL.type";
+import { HeroCarousel } from "@/components/HeroCarousel";
 
 export const CMSPage = ({
     content, bottom_content
 }: IRoute) => {
     return (
         <>
+            <HeroCarousel />
             <div dangerouslySetInnerHTML={{__html: content}}></div>
             <div dangerouslySetInnerHTML={{__html: bottom_content}}></div>
         </>
