504e7c6cecde70c7975d9e07838d716b0a26f026
Luan Nguyen Manh <luan.nguyen3@niteco.se>
2024-05-16 17:14:10 +0700

    Improve LCP for HeroCarousel.

--- src/components/HeroCarousel/HeroCarousel.tsx
+++ src/components/HeroCarousel/HeroCarousel.tsx
@@ -26,19 +26,26 @@ const HeroCarousel = () => {
                     {data?.items?.map((item, index) => (
                         <SwiperSlide key={index} virtualIndex={index} className="hero-banner-block height-full in-homepage">
                             <Link href="/" className={"d-block"}>
-                                <picture>
-                                    <source media="(max-width:767px)" srcSet={item.image_url_mobile?.src} />
-                                    <source media="(min-width:768px)" srcSet={item.image_url?.src} />
-                                    <Image
-                                        src={item.image_url_mobile?.src}
-                                        alt={item.name}
-                                        priority={index === 0 ? true : false}
-                                        width={0}
-                                        height={0}
-                                        sizes="100vw"
-                                        style={{ width: '100%', height: 'auto' }}
-                                    />
-                                </picture>
+                                <Image
+                                    className="d-none d-m-block"
+                                    src={item.image_url_mobile?.src}
+                                    alt={item.name}
+                                    priority={index === 0 ? true : false}
+                                    width={0}
+                                    height={0}
+                                    sizes="100vw"
+                                    style={{ width: '100%', height: 'auto' }}
+                                />
+                                <Image
+                                    className="d-m-none"
+                                    src={item.image_url?.src}
+                                    alt={item.name}
+                                    priority={index === 0 ? true : false}
+                                    width={0}
+                                    height={0}
+                                    sizes="100vw"
+                                    style={{ width: '100%', height: 'auto' }}
+                                />
                             </Link>
                         </SwiperSlide>
                     ))}
